ed4df453285540a7ce3f671fe1e0d97acb209cfd
Abraham Serrano <abrahamserrano@MacBook-Pro-de-Abraham-2.local>
2021-03-14 17:41:23 -0600

    first deploy

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const logger = require('./loggerMiddleware')
+
+app.use(cors())
+app.use(express.json())
+app.use(logger)
+
+let notes = [
+  {
+    id: 1,
+    content: 'Abraham Serrano Montiel',
+    date: '2019-05-30T',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Abraham Serrano Montiel',
+    date: '2019-05-30T',
+    important: true
+  },
+  {
+    id: 3,
+    content: 'Abraham Serrano Montiel',
+    date: '2019-05-30T',
+    important: true
+  }
+]
+
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { "Content-Type": "application/json" });
+//   response.end("Hello World");
+// });
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hellloooo</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find((note) => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter((note) => note.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+
+  if (!note || !note.content) {
+    return response.status(400).json({
+      error: 'note.content is missing'
+    })
+  }
+
+  console.log(note)
+
+  const ids = notes.map((note) => note.id)
+  const maxId = Math.max(...ids)
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== 'undefined' ? note.important : false,
+    date: new Date().toISOString()
+  }
+
+  notes = [...notes, newNote]
+
+  response.json(newNote)
+})
+
+app.use((request, response) => {
+  response.status(404).json({
+    error: 'Not Found'
+  })
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
